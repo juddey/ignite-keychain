3034c7168466eb4616250dfc48cb27de14110214
Justin Lane <justin@justinlane.org>
2017-03-06 15:14:27 +1300

    Adds Tests - all passing

--- test/add.js
+++ test/add.js
@@ -1,16 +1,47 @@
 const test = require('ava')
 const sinon = require('sinon')
-const plugin = require('../index')
+const plugin = require('../plugin')
 
-test('adds the proper npm module and copies example file', async t => {
+test('adds the proper npm module, copies file, and does not add to LoginSaga ', async t => {
   // spy on few things so we know they're called
   const addModule = sinon.spy()
   const addPluginComponentExample = sinon.spy()
+  const exists = sinon.stub().returns(false)
+  const patchInFile = sinon.spy()
+  const copy = sinon.spy()
+
   // mock a context
   const context = {
-    ignite: { addModule, addPluginComponentExample }
+    ignite: { addModule, addPluginComponentExample, patchInFile },
+    filesystem: { exists, copy }
   }
   await plugin.add(context)
 
+  // Check for some results.
   t.true(addModule.calledWith('react-native-keychain', { link: true }))
+  t.true(exists.calledTwice)
+  t.true(copy.called)
+  t.true(patchInFile.notCalled)
+})
+
+test('adds the proper npm module, does not copy file, and adds to LoginSaga ', async t => {
+  // spy on few things so we know they're called
+  const addModule = sinon.spy()
+  const addPluginComponentExample = sinon.spy()
+  const exists = sinon.stub().returns(true)
+  const patchInFile = sinon.spy()
+  const copy = sinon.spy()
+
+  // mock a context
+  const context = {
+    ignite: { addModule, addPluginComponentExample, patchInFile },
+    filesystem: { exists, copy }
+  }
+  await plugin.add(context)
+
+  // Check for some results.
+  t.true(addModule.calledWith('react-native-keychain', { link: true }))
+  t.true(exists.calledTwice)
+  t.true(copy.notCalled)
+  t.true(patchInFile.called)
 })
--- test/remove.js
+++ test/remove.js
@@ -1,16 +1,53 @@
 const test = require('ava')
 const sinon = require('sinon')
-const plugin = require('../index')
+const plugin = require('../plugin')
 
-test('removes Keychain', async t => {
+
+
+
+test('removes Keychain and sample code', async t => {
   const removeModule = sinon.spy()
-  const removePluginComponentExample = sinon.spy()
+  const confirm = sinon.stub().returns(true)
+  const remove = sinon.spy()
+  const patchInFile = sinon.spy()
+  const exists = sinon.stub().returns(true)
 
   const context = {
-    ignite: { removeModule, removePluginComponentExample }
+    ignite: { removeModule, patchInFile },
+    filesystem: { exists, remove },
+    prompt: { confirm }
   }
 
   await plugin.remove(context)
 
   t.true(removeModule.calledWith('react-native-keychain', { unlink: true }))
+  t.true(confirm.called)
+  t.true(remove.called)
+  t.true(patchInFile.called)
+  t.is(remove.args[0][0], `${process.cwd()}/App/Services/Keychain.js`)
+  t.is(patchInFile.args[0][0], `${process.cwd()}/App/Sagas/LoginSagas.js`)
+  t.is(patchInFile.args[0][1].delete, "import { storeAuth } from \'../Services/Keychain\'\n")
+})
+
+
+test('removes Keychain Module, but leaves sample code intact', async t => {
+  const removeModule = sinon.spy()
+  const confirm = sinon.stub().returns(false)
+  const remove = sinon.spy()
+  const patchInFile = sinon.spy()
+  const exists = sinon.spy()
+
+  const context = {
+    ignite: { removeModule, patchInFile },
+    filesystem: { exists, remove },
+    prompt: { confirm }
+  }
+
+  await plugin.remove(context)
+
+  t.true(removeModule.calledWith('react-native-keychain', { unlink: true }))
+  t.true(confirm.called)
+  t.true(remove.notCalled)
+  t.true(patchInFile.notCalled)
+  t.true(exists.notCalled)
 })
